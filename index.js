3cb14f3f18358a50dfb9283943a479b4580110ce
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-11 16:53:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // write your code below!
 
-Function happyHolidays 
+Function happyHolidays
 
-}
 Function happyHolidaysTo(name) {
   return string(`Happy holidays + ${name}!` )
 }
-Function happyCustomHolidayTo(holiday, name) {
+Function happyCustomHolidayTo{holiday}, {name} {
   return string(`Happy ${holiday}, ${name}!`)
 }
-Function holidayCountdown(holiday, days) {
+Function holidayCountdown({holiday}, {days}) {
   return string (`It,s ${days} days until ${holiday}!`)
 }
