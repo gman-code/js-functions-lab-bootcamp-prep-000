9c325731482dd9cdffc7cf66e61ea05fa8a2378c
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-11 16:22:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 // write your code below!
 
-function Happyholidays(string){
-return string. ()
-}
-function happyHolidaysTo(name){
-  return string.(`Happyholidays, ${name}!`)
-}
-function happyHolidaysTo(birthday)(name)
-  return string.`Happy ${holiday}, ${name}!`
-
-function holidayCountdown(july 3rd)(birthday){
-return string.` It,s ${days} day until ${holiday}!`
-}
